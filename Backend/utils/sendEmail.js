ac186f8169657cfda987793d9d52897a0db7db74
vishal123k <vishalk38916@gamil.com>
2026-02-11 15:55:03 +0530

    changes

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,20 +1,31 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service:"gmail",
-  auth:{
-    user:process.env.EMAIL,
-    pass:process.env.EMAIL_PASS
-  }
+  host: "smtp.gmail.com",
+  port: 587,          // ⭐ CHANGE THIS
+  secure: false,      // ⭐ MUST be false with 587
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-const sendEmail = async(to,subject,text)=>{
-  await transporter.sendMail({
-    from:process.env.EMAIL,
-    to,
-    subject,
-    text
-  });
+const sendEmail = async (to, subject, text) => {
+  try {
+
+    await transporter.sendMail({
+      from: `"PawMatch" <${process.env.EMAIL}>`,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("✅ Email sent");
+
+  } catch (err) {
+
+    console.log("❌ EMAIL ERROR 👉", err.message);
+  }
 };
 
 module.exports = sendEmail;
