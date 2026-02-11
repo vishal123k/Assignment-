3ea2e164d9e16187c8ad13513d920421bbc82355
vishal123k <vishalk38916@gamil.com>
2026-02-11 19:58:18 +0530

    fix email fix

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,29 +1,31 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 587,         
+  secure: false,      
   auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_PASS,
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
 const sendEmail = async (to, subject, text) => {
   try {
+
     await transporter.sendMail({
-      from: `"PawMatch" <${process.env.BREVO_USER}>`,
+      from: `"PawMatch" <${process.env.EMAIL}>`,
       to,
       subject,
       text,
     });
 
     console.log("Email sent");
+
   } catch (err) {
-    console.log("EMAIL ERROR:", err);
+
+    console.log("EMAIL ERROR", err.message);
   }
 };
 
 module.exports = sendEmail;
-
